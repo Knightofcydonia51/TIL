f0bc11f1770adb3a60d577756919d52883bc8163
bth <unknown>
2019-10-04 18:02:29 +0900

    19/10/04

--- 03_Django/04_django_crud_review/articles/views.py
+++ 03_Django/04_django_crud_review/articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ValidationError
 from .models import Article
 
 def index(request):
@@ -13,6 +14,20 @@ def new(request):
     return render(request, 'articles/new.html')
 
 def create(request):
+    try:
+        title=request.POST.get('title')
+        content=request.POST.get('content')
+        article= Article(title=title, content=content)
+        article.full_clean()
+
+    except ValidationError:
+        raise ValidationError('Your Error Message')
+    else:
+        article.save()
+        return redirect('articles:detail', article.pk)
+
+
+    '''
     #1. 첫번째 방법
     title=request.POST.get('title')
     print(title)
@@ -26,13 +41,36 @@ def create(request):
     # return render(request, 'articles/create.html')
 
     #2.두번째 방법
-    # article=Article(title=title, content=content)
-    # article.save()
+    article=Article(title=title, content=content)
+    article.save()
 
     #3. 세번째 방법
-    Article.objects.create(title=title, content=content)
+    # Article.objects.create(title=title, content=content)
 
     # return render(request, 'articles/index.html')
-    return redirect('/articles/')
+    return redirect(f'/articles/{article.pk}')
+    '''
 
-def detail(request)
+def detail(request,pk): #pk가 필요한 이유 : 특정한 글을 찾아내야하기 때문
+    article=Article.objects.get(pk=pk)
+    context={'article':article}
+    return render(request, 'articles/detail.html', context)
+
+def delete(request,pk):
+    article=Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+def edit(request,pk):
+    article=Article.objects.get(pk=pk)
+    context={'article':article}
+    return render(request, 'articles/edit.html',context)
+
+def update(request,pk):
+
+    article=Article.objects.get(pk=pk)
+    article.title=request.POST.get('title')
+    article.content=request.POST.get('content')
+    article.save()
+
+    return redirect('articles:detail', article.pk)
