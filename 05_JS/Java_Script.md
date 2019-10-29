e7e14380ca4c36f7c0c906751896ca5dfcfb12c0
Knightofcydonia51 <leavingwill@gmail.com>
2019-10-29 19:32:43 +0900

    19/10/29

---
+++ 05_JS/Java_Script.md
@@ -0,0 +1,22 @@
+# Java_Script
+
+>
+>
+>
+
+## extensions
+
+beautify
+
+auto close tag
+
+live server
+
+rainbow bracket
+
+indent rainbow
+
+
+
+
+
--- algorithm/BAEKJOON/9205.Beer.py
+++ algorithm/BAEKJOON/9205.Beer.py
@@ -1,5 +1,36 @@
-T=input(int())
+import sys
+sys.stdin=open('9205.Beer.txt')
 
-for i in range(T):
-    n=input(int())
-    for
+def perm(n,k):
+    global flag
+    if flag==1:
+        flag='happy'
+        return
+    if n==k:
+        for j in range(len(A)-1):
+            if A[j+1][0]-A[j][0]+A[j+1][1]-A[j][1]<=1000:
+                if j==len(A)-2:
+                    flag=1
+                continue
+            else:
+                break
+
+    for j in range(k,n):
+        A[k],A[j]=A[j],A[k]
+        perm(n,k+1)
+        A[k], A[j] = A[j], A[k]
+
+t=int(input())
+
+for i in range(t):
+    n=int(input())
+    A=[[int(x) for x in input().split()] for y in range(n+2)]
+    flag='sad'
+    perm(len(A)-1, 1)
+    print(flag)
+
+
+
+
+
+
