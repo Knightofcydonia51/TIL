1be42edc77a33b9b22a05ac6694828df62364dcf
Knightofcydonia51 <leavingwill@gmail.com>
2019-12-19 18:00:48 +0900

    19/12/19

--- algorithm/BAEKJOON/14501.quit.py
+++ algorithm/BAEKJOON/14501.quit.py
@@ -5,21 +5,19 @@ def powerset(r,k,ans):
     global maxi
     if ans > maxi:
         maxi = ans
-
-    if r>=N or k>=N:return
-    if data[k][0] > N - r: return
-        # 남은 기간보다 상담 기간이 길때
-        #남은 날짜보다 상담 기간이 길때?
-        # powerset(r, k + 1, ans)
-        # data 내에서 해당 데이터의 인덱스값으로부터 시작해서 일수만큼 반복
-    for i in range(data[k][0]):
-        T[r+i]=1
-    powerset(r+data[k][0],k+data[k][0],ans+data[k][1])
-    for i in range(data[k][0]):
-        T[r + i] = 0
-    powerset(r+1,k+1,ans)
-
-
+    if r>=N:return
+    if data[k][0] > N - r and k!=0:return
+    if k==0 and data[k][0]>N-r:
+        powerset(r+1,k+1,ans)
+    else:
+        # 남은 기간보다 상담 기간이 길때 return but k가 끝까지 순회하지 않았다면
+        # 끝까지 봐야함
+        for i in range(data[k][0]):
+            T[r+i]=1
+        powerset(r+data[k][0],k+data[k][0],ans+data[k][1])
+        for i in range(data[k][0]):
+            T[r + i] = 0
+        powerset(r+1,k+1,ans)
 
 
 N=int(input())
