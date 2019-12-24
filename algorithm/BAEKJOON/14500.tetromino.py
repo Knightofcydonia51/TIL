d600abc03681b1bde24cceaa1fd5d79dd5e4e944
배태한 <leavingwill@gmail.com>
2019-12-24 16:43:21 +0900

    191224

--- algorithm/BAEKJOON/14500.tetromino.py
+++ algorithm/BAEKJOON/14500.tetromino.py
@@ -1,9 +1,21 @@
 import sys
 
+# ㄴ
 sero1_yx=[[0,0],[-1,0],[-1,0],[0,1]]
 garo1_yx=[[0,0],[-1,0],[0,-1],[0,-1]]
 rsero1_yx=[[0,0],[-1,0],[-1,0],[0,-1]]
-rgaro2_yx=[]
+rgaro2_yx=[[0,0],[0,1],[0,1],[1,0]]
+
+# thunder
+th_sero_yx=[[0,0],[1,0],[0,1],[1,0]]
+th_garo_yx=[[0,0],[0,-1],[-1,0],[0,-1]]
+th_rsero_yx=[[0,0],[1,0],[0,-1],[1,0]]
+
+# ㅗ
+mountain_garo_yx=[[0,0],[1,-1],[1,0],[1,1]]
+mountain_left_yx=[[0,0],[-1,-1],[0,-1],[1,-1]]
+
+
 
 def sero_stick(y,x):
     hap=0
@@ -42,7 +54,7 @@ def garo1_niun(y,x): # 가로가 긴 ㄴ 대칭
         hap += sheet[y + garo1_yx[i][0]][x + garo1_yx[i][1]]
     return hap
 
-def garo2_niun(y,x): # 가로가 긴 ㄱ 대칭
+def garo2_niun(y,x): # 가로가 긴 ㄱ 대칭 (역방향)
     hap=0
     for i in range(4):
         hap += sheet[y + sero1_yx[i][1]][x + sero1_yx[i][0]]
@@ -54,10 +66,10 @@ def rsero1_niun(y,x): # 세로가 긴 ㄴ 대칭
         hap += sheet[y + rsero1_yx[i][0]][x + rsero1_yx[i][1]]
     return hap
 
-def rsero2_niun(y,x):
+def rsero2_niun(y,x): #세로가 긴 ㄱ 대칭 (역방향)
     hap = 0
     for i in range(4):
-        hap += sheet[y + garo1_yx[i][1]][x + garo1_yx[i][0]]
+        hap += sheet[y + sero1_yx[i][0]][x + sero1_yx[i][1]]
     return hap
 
 def rgaro1_niun(y,x): # 가로 긴 ㄴ모양 (역방향탐색)
@@ -67,11 +79,64 @@ def rgaro1_niun(y,x): # 가로 긴 ㄴ모양 (역방향탐색)
     return hap
 
 def rgaro2_niun(y,x): # 가로 긴 ㄱ모양
-    
+    hap = 0
+    for i in range(4):
+        hap += sheet[y + rgaro2_yx[i][0]][x + rgaro2_yx[i][1]]
+    return hap
+
+def th_sero(y,x):
+    hap=0
+    for i in range(4):
+        hap+=sheet[y+th_sero_yx[i][0]][x+th_sero_yx[i][1]]
+    return hap
+
+def th_garo(y,x):
+    hap = 0
+    for i in range(4):
+        hap += sheet[y + th_garo_yx[i][0]][x + th_garo_yx[i][1]]
+    return hap
+
+def th_rsero(y,x):
+    hap = 0
+    for i in range(4):
+        hap += sheet[y + th_rsero[i][0]][x + th_rsero_yx[i][1]]
+    return hap
+
+def th_rgaro(y,x): #
+    hap = 0
+    for i in range(4):
+        hap += sheet[y + th_sero_yx[i][1]][x + th_sero_yx[i][0]]
+    return hap
+
+def mountain_garo(y,x):
+    hap = 0
+    for i in range(4):
+        hap += sheet[y + mountain_garo_yx[i][0]][x + mountain_garo_yx[i][1]]
+    return hap
+
+def mountain_left(y,x):
+    hap = 0
+    for i in range(4):
+        hap += sheet[y + mountain_left_yx[i][0]][x + mountain_left_yx[i][1]]
+    return hap
+
+def mountain_reverse(y,x):
+    hap = 0
+    for i in range(4):
+        hap += sheet[y + mountain_left_yx[i][1]][x + mountain_left_yx[i][0]]
+    return hap
+
+def mountain_right(y,x):
+    hap = 0
+    for i in range(4):
+        hap += sheet[y + mountain_garo_yx[i][1]][x + mountain_garo_yx[i][0]]
+    return hap
 
 N,M=map(int,input())
 sheet=[[int(x) for x in input().split()]for y in range(N)]
-
+for y in range(N):
+    for x in range(M):
+        
 
 
 
