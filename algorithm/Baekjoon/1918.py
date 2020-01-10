a4f6a5a123d00c7335d7d3c4785548af767a8bb3
배태한 <leavingwill@gmail.com>
2020-01-10 17:55:49 +0900

    200110

--- algorithm/Baekjoon/1918.py
+++ algorithm/Baekjoon/1918.py
@@ -1,23 +1,10 @@
-import sys
-sys.stdin=open("tomato.txt")
-def comb(r,k):
-    if r==3:
-        for i in range(len(T)):
-            print(T[i],end=" ")
-        print()
-    if k>len(data)-1 or r>len(T)-1:
-        return
-    else:
-        T[r]=data[k]
-        comb(r+1,k+1)
-        comb(r,k+1)
+N = 4
+cnt = 0
+def dfs():
+    global cnt
+    print(1)
+    cnt += 1
+    if cnt < N:
+        dfs()
+dfs()
 
-
-data=[int(x) for x in range(6)]
-T=[0]*3
-comb(0,0)
-
-ans= -1
-N=int(input())
-people=[int(x) for x in input().split()]
-node=[list(map(int, input().split())) for _ in range(N)]
