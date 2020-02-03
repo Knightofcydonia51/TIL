55d161b16a7a0b35b7cbfe65c996bf8eca4439b6
배태한 <leavingwill@gmail.com>
2020-02-03 19:50:07 +0900

    BOJ and vue

---
+++ 06_Vue/07_vue_image/my-project/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  
+
+<div>
+    <input type="file" name="image" @change="GetImage">
+    <img :src="avatar" alt="Image">
+    <a href="#" class="btn btn-success" @click.prevent="upload">등록</a>
+</div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      avatar:null
+    }
+  },
+  methods:{
+    GetImage(e){
+      let image = e.target.files[0]
+      let reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = e =>{
+        this.avatar=e.target.result
+      }
+      console.log(e.target.files)
+    },
+    upload(){
+      axios.post('/upload',{'image': this.avatar})
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
