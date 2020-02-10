880715eafa44e670758bc3ae90229db0083c1d97
배태한 <leavingwill@gmail.com>
2020-02-10 19:26:59 +0900

    BOJ 17281

--- algorithm/BOJ/17281.baseball.py
+++ algorithm/BOJ/17281.baseball.py
@@ -1,33 +1,75 @@
-# import sys
-# sys.stdin=open('17281.baseball.txt')
-#
-# def comb(r,k): # 빈칸의 포인터, 풀의 포인터
-#     global maxi
-#     if r==0:
-#         pass #야구 실행
-#
-#     else:
-#         if r>k:
-#             return
-#         else:
-#             T[r-1]=A[k-1]
-#             comb(r-1,k-1) #선택시
-#             comb(r,k-1) #선택안할시
-#
-#
-# # 타순을 정하고, 타순이 정해지면 야구 실행
-# #
-#
-# N= int(input())
-# innings=[list(map(int,input().split())) for x in range(N)]
-# ans=0
-# for i in range(len(innings)):
-#     maxi=0
-#     A=innings[i]
-#     T=[0]*9
-# #     comb,maxi
+import sys
+sys.stdin=open('17281.baseball.txt')
+
+import collections
+import copy
 
 
-A=[1,2,3,4]
-A.append()
-print(A)
+def game():
+    global zeros
+    global maxi
+    bases=collections.deque([0,0,0])
+    score=0
+    out=0
+    for i in range(zeros):
+        T.append(0)
+    T.appendleft(A[0])
+    T[3], T[0] = T[0], T[3]
+    print(T)
+    for i in range(len(T)):
+        if T[i]:
+            bases.appendleft(1)
+            if bases.pop():
+                score += 1
+            else:
+                pass
+            for k in range(T[i]-1):
+                bases.appendleft(0)
+                if bases.pop():
+                    score += 1
+                else:
+                    continue
+
+        else:
+            out+=1
+            if out==3:
+                break
+
+    if score>maxi:
+        maxi=score
+
+
+def perm(k):
+    if k==len(T):
+        game()
+    else:
+        for i in range(k,len(A)):
+            T[k],T[i]=T[i],T[k]
+            perm(k+1)
+            T[k], T[i] = T[i], T[k]
+
+
+N= int(input())
+innings=[list(map(int,input().split())) for x in range(N)]
+ans=0
+maxi=0
+
+for i in range(len(innings)):
+    maxi=0
+    A=innings[i]
+    zeros=0
+    T = collections.deque([])
+    for k in range(1,9):
+        if A[k]:
+            T.append(A[k])
+        else:
+            zeros+=1
+
+    perm(0)
+
+print(maxi)
+
+
+
+
+
