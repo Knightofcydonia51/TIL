d64d0572a60168dea25105fd1b5f910cb085b3b0
Knightofcydonia51 <leavingwill@gmail.com>
2020-02-12 22:18:05 +0900

    .

--- algorithm/BOJ/17281.baseball.py
+++ algorithm/BOJ/17281.baseball.py
@@ -1,33 +1,49 @@
-# import sys
-# sys.stdin=open('17281.baseball.txt')
-#
-# def comb(r,k): # 빈칸의 포인터, 풀의 포인터
-#     global maxi
-#     if r==0:
-#         pass #야구 실행
-#
-#     else:
-#         if r>k:
-#             return
-#         else:
-#             T[r-1]=A[k-1]
-#             comb(r-1,k-1) #선택시
-#             comb(r,k-1) #선택안할시
-#
-#
-# # 타순을 정하고, 타순이 정해지면 야구 실행
-# #
-#
-# N= int(input())
-# innings=[list(map(int,input().split())) for x in range(N)]
-# ans=0
-# for i in range(len(innings)):
-#     maxi=0
-#     A=innings[i]
-#     T=[0]*9
-# #     comb,maxi
+import sys
+sys.stdin=open('17281.baseball.txt')
+
+from copy import deepcopy
+from collections import deque
+
+def game(T):
+    base=deque([])
+    out=0
+    while out<3:
+        for i in range(9):
+            if T[i]:
+                base.appendleft(1)
+                for k in range(T[i]-1):
+                    base.appendleft(0)
+            else:
+                out+=1
+                if out==3:
+                    break
 
 
-A=[1,2,3,4]
-A.append()
-print(A)
+
+def perm(k):
+    global one
+    if k==8:
+        T=deepcopy(A)
+        T.append(one)
+        T[3],T[8]=T[8],T[3]
+        base=[]
+
+
+
+    else:
+        for i in range(k,8):
+            A[k],A[i]=A[i],A[k]
+            perm(k+1)
+            A[k], A[i] = A[i], A[k]
+
+N= int(input())
+innings=[list(map(int,input().split())) for x in range(N)]
+ans=0
+
+for i in range(len(innings)):
+    maxi=0
+    A=innings[i][1:]
+    one=innings[i][0]
+    perm(0)
+
+
