0f6c636eb3f6b3cc42f50643a067a61d0398bec7
배태한 <leavingwill@gmail.com>
2020-06-24 02:00:07 +0900

    [Add] chart.js

---
+++ 00_Documents/08_Vue.md
@@ -0,0 +1,40 @@
+## Node.js와 vue를 활용한 프론트엔드 프로젝트 관리
+
+- Node.js는 설치되어 있다는 가정 하에 진행
+
+
+
+
+
+### 1. vue-cli3 설치
+
+```
+$ npm i -g @vue/cli
+$ vue --version
+```
+
+
+
+### 2. vue 프로젝트 생성
+
+```
+vue create <프로젝트 이름>
+```
+
+- default와 수동 설정이 나오는데, 원하는 항목을 선택한다.
+
+
+
+### 3. 서버 실행
+
+```
+# package.json 이 있는 경로에서
+npm install
+npm run serve
+```
+
+
+
+### 4. 명령어 일람
+
+- vue ui : 프로젝트 관리하는 웹 띄우는 기능
--- 05_Vue/08_vue_graph/graph/src/App.vue
+++ 05_Vue/08_vue_graph/graph/src/App.vue
@@ -1,24 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1> cos_name </h1>
+    <div id="app"  style="width:400px;">
+      <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
+    </div>
+    평균값  : {{average(chartData.datasets[0].data)}}
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import PieChart from "./PieChart.js";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    PieChart
+  },
+  methods:{
+    average(array){
+      var sum = 0.0;
+
+      for (var i = 0; i < array.length; i++)
+        sum += array[i];
+      
+      return sum / array.length;
+   }
+  },
+  data() {
+    return {
+      chartOptions: {
+        hoverBorderWidth: 20
+      },
+      chartData: {
+        hoverBackgroundColor: "red",
+        hoverBorderWidth: 10,
+        labels: ["유해도 1", "유해도 2", "유해도 3"],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: ["#41B883", "#E46651", "#00D8FF"],
+            data: [1, 3, 4]
+          }
+        ]
+      }
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -26,3 +58,6 @@ export default {
   margin-top: 60px;
 }
 </style>
+
+
+
